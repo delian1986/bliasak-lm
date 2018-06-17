69cd5a204ca64b941359b94230c71930869c856c
delian1986 <delian1914@gmail.com>
2018-06-17 12:50:35 +0300

    structure edited

--- js/scroll-down.js
+++ js/scroll-down.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("a").on('click', function(event) {
         if (this.hash !== "") {
             event.preventDefault();
-            var hash = this.hash;
+            let hash = this.hash;
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
             }, 800, function(){
